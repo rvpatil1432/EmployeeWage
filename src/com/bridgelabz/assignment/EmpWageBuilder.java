909325494f421c1622bceb567593796763ae746a
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-10 19:02:11 +0530

    Adding : welcome message to employee wage program

---
+++ src/com/bridgelabz/assignment/EmpWageBuilder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.assignment;
+
+public class EmpWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program..");
+	}
+}
